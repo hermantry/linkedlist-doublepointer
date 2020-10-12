08f9c6e74cbb4c8dcfad8e63e5ffe70f93f4d35a
Herman Try <111201811499@mhs.dinus.ac.id>
2020-10-12 22:29:06 +0700

    Procedural

---
+++ list.h
@@ -0,0 +1,56 @@
+#ifndef LIST_H_INCLUDED
+#define LIST_H_INCLUDED
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define true 1
+#define false 0
+
+#define boolean unsigned char
+#define Nil NULL
+
+typedef int infotype;
+typedef struct tElmtList *address;
+
+typedef struct tElmtList {
+    infotype Info;
+    address Prev;
+    address Next;
+}ElmtList;
+
+typedef struct {
+    address First;
+    address Last;
+}List;
+
+#define Info(P) (P)->Info
+#define Prev(P) (P)->Prev
+#define Next(P) (P)->Next
+
+#define First(L) (L).First
+#define Last(L) (L).Last
+
+address Alokasi(infotype X);
+address Search(List L, infotype X);
+
+boolean ListEmpty(List L);
+boolean FSearch(List L, address P);
+
+void CreateList(List *L);
+
+void Dealokasi(address *P);
+void DelFirst(List *L, address *P);
+void DelLast(List *L, address *P);
+void DelVFirst(List *L, infotype *X);
+void DelVLast(List *L, infotype *X);
+
+void InsVFirst(List *L, infotype X);
+void InsVLast(List *L, infotype X);
+void InsertFirst(List *L, address P);
+void InsertAfter(List *L, address P, address Prec);
+void InsertLast(List *L, address P);
+
+void PrintInfo(List L);
+
+#endif // LIST_H_INCLUDED
---
+++ main.c
@@ -0,0 +1,85 @@
+#include "List.h"
+
+int main() {
+    List L;
+    address P;
+    infotype X;
+
+    int i, N;
+
+    printf("============================================================================\n");
+    printf("     =================== LINKED LIST DOUBLE POINTER ===================\n");
+    printf("============================================================================\n\n");
+
+    CreateList(&L);
+
+    printf("\n======================== Insert Elemen Di Awal List ========================\n");
+    printf("* Banyaknya elemen yang ditambahkan : ");
+    scanf("%d", &N);
+
+    for (i=1; i<=N; i++) {
+        printf("  - Input elemen List : ");
+        scanf("%d", &X);
+        InsVFirst(&L, X);
+    }
+
+    printf("* List L : ");
+    PrintInfo(L);
+
+    printf("\n\n======================== Delete Elemen Di Awal List ========================\n");
+    printf("* Banyaknya elemen yang dihapus : ");
+    scanf("%d", &N);
+
+    printf("* Elemen yang dihapus : ");
+
+    for (i=1; i<=N; i++) {
+        DelVFirst(&L, &X);
+        printf("|%d| ", X);
+    }
+
+    printf("\n* List L : ");
+    PrintInfo(L);
+
+    printf("\n\n======================= Insert Elemen Di Akhir List ========================\n");
+    printf("* Banyaknya elemen yang ditambahkan : ");
+    scanf("%d", &N);
+
+    for (i=1; i<=N; i++) {
+        printf("  - Input elemen List : ");
+        scanf("%d", &X);
+        InsVLast(&L, X);
+    }
+
+    printf("* List L : ");
+    PrintInfo(L);
+
+    printf("\n\n======================= Delete Elemen Di Akhir List ========================\n");
+    printf("* Banyaknya elemen yang dihapus : ");
+    scanf("%d", &N);
+
+    printf("* Elemen yang dihapus : ");
+
+    for (i=1; i<=N; i++) {
+        DelVLast(&L, &X);
+        printf("|%d| ", X);
+    }
+
+    printf("\n* List L : ");
+    PrintInfo(L);
+
+    printf("\n\n=========================== Search Elemen List =============================\n");
+    printf("* Masukkan elemen yang ingin dicari : ");
+    scanf("%d", &X);
+
+    printf("* Hasil pencarian : ");
+    P = Search(L, X);
+
+    if (P != Nil)
+        printf("Elemen ditemukan pada alamat 0x%p", P);
+    else
+        printf("Elemen tidak ditemukan");
+
+    printf("\n\n============================================================================\n");
+
+    return 0;
+}
